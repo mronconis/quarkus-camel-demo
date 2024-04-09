9e7a5245611cc4ccd9a54b74cf62c2c98cf3fc03
Scaffolder <scaffolder@backstage.io>
2024-04-09 19:33:24 +0000

    initial commit

---
+++ src/main/java/org/acme/quarkus/camel/demo/TimerRoute.java
@@ -0,0 +1,20 @@
+package org.acme.quarkus.camel.demo;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import static java.lang.String.format;
+
+@ApplicationScoped
+public class TimerRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("timer:foo?period={{timer.period}}")
+                .log("Fire timer!")
+                .to("log:timer");
+    }
+}
+
